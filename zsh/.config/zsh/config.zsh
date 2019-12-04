1e7e6d8be6e8dfb4f5149ad4fb17a8c28995beb8
blackdracula <Abubakaryusuf951@gmail.com>
2019-12-04 17:29:58 +0800

    zsh: create some aliases am too lazy now

--- zsh/.config/zsh/config.zsh
+++ zsh/.config/zsh/config.zsh
@@ -34,6 +34,14 @@ sudo-command-line() {
 zle -N sudo-command-line
 bindkey "\e\e" sudo-command-line
 
+tmux set -g status-right "#(echo \\\\(test\\\\))"
+tmux set -g status-bg \#0f111a
+
+#determines search program for fzf
+if type ag &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='ag --hidden -p ~/.fignore -g ""'
+fi
+
 # various aliases.
 alias free="free -h"
 alias network="sc-restart NetworkManager"
@@ -45,9 +53,8 @@ alias cpr="rsync --progress --size-only --inplace --verbose "
 alias cat="bat " # this one is way better
 alias ls="alias ls=lsd && ls"
 alias clip='xclip -selection clipboard'
-alias zsh="exec zsh"
 alias n="nvim"
-alias home="cd; clear"
+alias home="cd&&clear"
 alias suspend="systemctl suspend"
 alias C="cd; cd C_C++/C; ls"
 alias Cp="cd; cd C_C++/C++; ls"
@@ -55,12 +62,8 @@ alias scan="nmcli d wifi rescan"
 alias list="nmcli d wifi list"
 alias study="cd;cd Sem && ranger"
 alias jfx="javafx11-scenebuilder"
-alias gcc="g++"
-alias clean_history="cd; cat /dev/null >! .zhistory"
+alias ch="cd; cat /dev/null >! .zhistory"
 alias temp="System_temp.sh"
-alias c="bat"
-alias v="tmux split -v"
-alias h="tmux split -h"
 
 #packages aliases.
 alias remove="yay -Rs"
@@ -72,16 +75,18 @@ alias cleanc="yay -Scc" # clean cached packages files.
 # git aliases.
 alias gs="git status"
 alias ga="git add"
-alias gaa="git add -A"
 alias gc="git commit --signoff"
 alias gcp="git cherry-pick"
 alias gl="git log | bat"
 alias gd="git diff"
 alias gpush="git push origin master"
-alias gpull="git pull origin master"
+alias clone="git clone"
+alias gb="git branch -a"
+alias gco="git checkout"
+alias gbd="git branch -d "
 
 #export PATH && Vars
-export PATH=$PATH:/home/blackdracula/bin
+export PATH=$PATH:/home/blackdracula/.config/bin
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export EDITOR=nvim
@@ -91,3 +96,6 @@ wmname compiz
 
 #nice looking shit
 ufetch
+sleep 0.2
+#have no idea wtf is this not working well
+tmux source ~/Dotfiles/tmux/.config/tmux/.tmux.conf
