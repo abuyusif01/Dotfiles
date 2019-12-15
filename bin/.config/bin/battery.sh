24797235030c511bba8cc1251b1f27a7a7f10731
blackdracula18 <Abubakaryusuf951@gmail.com>
2019-12-15 23:49:35 +0800

    bin: remove useless endif

--- bin/.config/bin/battery.sh
+++ bin/.config/bin/battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Show the system battery percentage
-# Blackdracula
+# blackdracula18
 
 CHARGE=charging
 battery=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state|to\ full|to\ empty|percentage" | tail -n 1 | awk '{ print $2 }' | sed 's/%//')
@@ -15,8 +15,3 @@ fi
 if [ $battery_charging == $CHARGE ]; then
 echo "$battery% $battery_charging"
 fi
-# else
-# echo "$battery%Remain"
-# fi
-
-
