ef89d0a2df1240b2685978700c7f991cfd193484
blackdracula18 <Abubakaryusuf951@gmail.com>
2019-12-23 22:03:10 +0800

    zsh: edit the whole thing

--- zsh/.config/zsh/config.zsh
+++ zsh/.config/zsh/config.zsh
@@ -1,22 +1,8 @@
-# systemd aliases.
- user_commands=(
-  list-units is-active status show help list-unit-files
-  is-enabled list-jobs show-environment cat list-timers)
 
-sudo_commands=(
-  start stop reload restart try-restart isolate kill
-  reset-failed enable disable reenable preset mask unmask
-  link load cancel set-environment unset-environment
-  edit)
+if type ag &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='ag --hidden -p ~/.fignore -g ""'
+fi
 
-for c in $user_commands; do; alias sc-$c="systemctl $c"; done
-for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
-
-alias sc-enable-now="sc-enable --now"
-alias sc-disable-now="sc-disable --now"
-alias sc-mask-now="sc-mask --now"
-
-# double press Esc to add sudo.
 sudo-command-line() {
     [[ -z $BUFFER ]] && zle up-history
     if [[ $BUFFER == sudo\ * ]]; then
@@ -31,36 +17,17 @@ sudo-command-line() {
         LBUFFER="sudo $LBUFFER"
     fi
 }
-zle -N sudo-command-line
-bindkey "\e\e" sudo-command-line
-
-tmux set -g status-right "#(echo \\\\(test\\\\))"
-tmux set -g status-bg \#0f111a
-
-#determines search program for fzf
-if type ag &> /dev/null; then
-    export FZF_DEFAULT_COMMAND='ag --hidden -p ~/.fignore -g ""'
-fi
 
 # various aliases.
 alias free="free -h"
-alias network="sc-restart NetworkManager"
-alias blame="systemd-analyze && systemd-analyze blame"
-alias journal="journalctl -b0 -p err"
-alias fdisk="sudo fdisk -l"
-alias cleanj="sudo journalctl --vacuum-time=5d"
-alias cpr="rsync --progress --size-only --inplace --verbose "
 alias cat="bat " # this one is way better
 alias ls="alias ls=lsd && ls"
 alias clip='xclip -selection clipboard'
 alias n="nvim"
 alias home="cd&&clear"
 alias suspend="systemctl suspend"
-alias C="cd; cd C_C++/C; ls"
-alias Cp="cd; cd C_C++/C++; ls"
 alias scan="nmcli d wifi rescan"
 alias list="nmcli d wifi list"
-alias study="cd;cd Sem && ranger"
 alias ch="cd; cat /dev/null >! .zhistory"
 alias temp="System_temp.sh"
 
@@ -83,18 +50,3 @@ alias clone="git clone"
 alias gb="git branch -a"
 alias gco="git checkout"
 alias gbd="git branch -d "
-
-#export PATH && Vars
-export PATH=$PATH:/home/blackdracula/.config/bin
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export EDITOR=nvim
-
-#fuck intellij-idea
-wmname compiz
-
-#nice looking shit
-ufetch
-sleep 0.1
-#have no idea wtf is this not working well
-tmux source ~/Dotfiles/tmux/.config/tmux/.tmux.conf
