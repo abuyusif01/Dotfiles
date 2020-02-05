459ba37f286e8051d689147d217ebbcf41abc5c3
blackdracula18 <Abubakaryusuf951@gmail.com>
2020-02-06 01:18:20 +0800

    bin: delete some bullshit

--- bin/.config/bin/fzf.sh
+++
@@ -1,3 +0,0 @@
-se (){du -a (dir) | awk '{print $3}' | fzf | xargs -r $EDITOR;}
-sv (){vcopy "$(du -a (dir) | awk "{print $#}" | fzf )";}
-vf(fzf | xargs -r -I % $EDITOR %;)
--- bin/.config/bin/te.sh
+++
@@ -1,8 +0,0 @@
-for ((i=1; i<=10; i++))
-	do
-	if [[ $(( $i % 2 )) == 0 ]]; then
-		echo "even $i"
-	else 
-	echo "odd $i"
-	fi
-done
