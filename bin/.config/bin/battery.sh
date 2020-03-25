4d889dbc708b817e5b9f465b716bb45304cabf09
blackdracula18 <Abubakaryusuf951@gmail.com>
2020-03-26 04:12:24 +0800

    bin: technically dunno how tf am using the old script hahaha

--- bin/.config/bin/battery.sh
+++ bin/.config/bin/battery.sh
@@ -3,15 +3,18 @@
 # Show the system battery percentage
 # blackdracula18
 
-CHARGE=charging
-battery=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state|to\ full|to\ empty|percentage" | tail -n 1 | awk '{ print $2 }' | sed 's/%//')
-battery_charging=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state|to\ full|to\ empty|percentage" | head -n 1 | awk '{ print $2 }')
+CHARGE=Charging
+battery=`cat /sys/class/power_supply/BAT0/capacity`
+battery_charging=`cat /sys/class/power_supply/BAT0/status`
+
 if [ $battery -eq 100 ]; then
-echo "fully-charged 100%"
+    echo "fully-charged 100%"
 fi
-if [ $battery_charging == discharging ]; then
-echo "$battery% remaining"
+
+if [ $battery_charging == Discharging ]; then
+    echo "$battery% remaining"
 fi
+
 if [ $battery_charging == $CHARGE ]; then
-echo "$battery% $battery_charging"
+    echo "$battery% $battery_charging"
 fi
