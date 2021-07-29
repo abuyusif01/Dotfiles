213adcb21e5e20eeb930a8216da26fe27de3369f
blackdracula18 <abubakaryusuf951@gmail.com>
2021-07-29 22:25:44 +0800

    to use and upload rev shell
    Signed-off-by: blackdracula18 <abubakaryusuf951@gmail.com>

--- bin/.config/bin/initial.sh
+++ bin/.config/bin/initial.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 user=$(whoami)
-get_all_packages()
-{
-    wget "https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd-musl_0.20.1_amd64.deb"
-    wget
-    "https://raw.githubusercontent.com/blackdracula18/Dotfiles/master/tmux/.tmux.conf"
-    ~/.tmux.conf
-}
-
 
 if [[ $user != "root" ]]; then
     echo -n "Only root can run this. sorry :("
     exit;
 fi
+
+#Geting some stuff from internet
+wget "https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd-musl_0.20.1_amd64.deb"
+wget "https://raw.githubusercontent.com/blackdracula18/Dotfiles/master/tmux/.tmux.conf"
+git clone https://github.com/maurosoria/dirsearch /opt/dirsearch
 
 dpkg -i lsd_0.20.1_amd64.deb
 sed -i "s/#Port 22/Port 443/g" /etc/ssh/sshd_config
