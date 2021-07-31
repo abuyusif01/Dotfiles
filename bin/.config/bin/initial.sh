b158cfa4607106b7031748e23f94dedc65661adc
blackdracula18 <abubakaryusuf951@gmail.com>
2021-07-31 07:09:49 -0400

    fix init

--- bin/.config/bin/initial.sh
+++ bin/.config/bin/initial.sh
@@ -12,8 +12,8 @@ wget "https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd-musl_0.20.1_a
 wget "https://raw.githubusercontent.com/blackdracula18/Dotfiles/master/tmux/.tmux.conf"
 git clone https://github.com/maurosoria/dirsearch /opt/dirsearch
 
-dpkg -i lsd_0.20.1_amd64.deb
-sed -i "s/#Port 22/Port 443/g" /etc/ssh/sshd_config
-sed -i "s/#ClientAliveInterval 1200/ClientAliveInterval 10000000/g" /etc/ssh/sshd_config
+#dpkg -i lsd_0.20.1_amd64.deb
+echo "Port 443" >> /etc/ssh/sshd_config
+#sed -i "s/#ClientAliveInterval 1200/ClientAliveInterval 10000000/g" /etc/ssh/sshd_config
 systemctl stop sshd
 systemctl start sshd
